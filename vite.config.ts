16dda08676f7fc784ccc91579c9d7bedcd2cc33f
DarkstarV1 <pranay.bodducherla@gmail.com>
2026-05-13 17:19:40 -0700

    Refactor tanstackStart configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  tanstackStart: {
-    target: "vercel",
-    server: { entry: "server" },
-  },
+tanstackStart({
+  customViteReactPlugin: true,
+  target: "vercel",
+})
+
 });
